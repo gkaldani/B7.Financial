2f5d5655166c328fd997332433b739d0b66e890e
Giorgi Kaldani <g.kaldani@altasoft.ge>
2025-09-24 00:24:59 +0400

    Net 10

---
+++ B7.Financial.Basics/B7.Financial.Abstractions/Index/IFloatingRate.cs
@@ -0,0 +1,9 @@
+﻿namespace B7.Financial.Abstractions.Index;
+
+/// <summary>
+/// An index or group of indices used to provide floating rates, typically in interest rate swaps.
+/// </summary>
+public interface IFloatingRate : INamed
+{
+    
+}
---
+++ B7.Financial.Basics/B7.Financial.Abstractions/Index/IIndex.cs
@@ -0,0 +1,10 @@
+﻿namespace B7.Financial.Abstractions.Index;
+
+/// <summary>
+/// An index of values, such as LIBOR, FED FUND or daily exchange rates.
+/// <para>
+/// An index is an agreed mechanism for determining certain financial indicators, <br/>
+/// such as exchange rate or interest rates. Most common indices are daily.
+/// </para>
+/// </summary>
+public interface IIndex : INamed;
